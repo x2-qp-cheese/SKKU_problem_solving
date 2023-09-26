5edf8b899ecab8c6d5224696b2f2b0c50b14af24
GEONWOO PARK <101126026+x2-qp-cheese@users.noreply.github.com>
2023-09-26 15:48:47 +0900

    Time: 2 ms (92.19%), Space: 6.7 MB (43.75%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.c
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.c
@@ -1,22 +1,24 @@
-int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize){
-    int* res;
-    if(nums2Size > nums1Size)
-        res = (int*)malloc(sizeof(int)*(nums2Size));
-    else
-        res = (int*)malloc(sizeof(int)*(nums1Size));
-                           
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    // 해시 테이블을 초기화하고 최대 배열 크기를 계산합니다.
+    int hashtable[1001] = {0};
+    int maxsize = (nums1Size > nums2Size ? nums1Size : nums2Size);
+
+    // 결과 배열을 할당하고 반환 크기를 초기화합니다.
+    int* res = (int*)malloc(sizeof(int) * maxsize);
     *returnSize = 0;
-    
-    for(int i = 0; i < nums1Size; i++){
-        for(int j = 0; j < nums2Size; j++){
-            if(nums1[i] == nums2[j]){
-                res[(*returnSize)++] = nums1[i];
-                nums2[j] = -1;
-                break;
-            }
+
+    // nums1 배열을 해시 테이블에 저장합니다.
+    for (int i = 0; i < nums1Size; i++) {
+        hashtable[nums1[i]]++;
+    }
+
+    // nums2 배열을 순회하면서 중복된 요소를 찾아 결과 배열에 추가합니다.
+    for (int j = 0; j < nums2Size; j++) {
+        if (hashtable[nums2[j]] > 0) {
+            res[(*returnSize)++] = nums2[j];
+            hashtable[nums2[j]]--; // 중복 체크를 하지 않으므로 해시 테이블 값을 감소시키지 않아도 됩니다.
         }
     }
-    
-    
+
     return res;
 }
