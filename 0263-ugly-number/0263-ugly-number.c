47422be8e2b4f6166cf8fbbdf00583f109fa8083
GEONWOO PARK <101126026+x2-qp-cheese@users.noreply.github.com>
2023-10-14 23:16:14 +0900

    Time: 0 ms (100.00%), Space: 6.3 MB (11.32%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.c
+++ 0263-ugly-number/0263-ugly-number.c
@@ -1,18 +1,19 @@
 bool isUgly(int n){
-    if (n > 0){
-        while(n > 1){
-        if (n%2 == 0)
+    if(n > 0)
+        {
+        while(n%2 == 0)
             n/=2;
-        else if (n%3 == 0){
+
+        while(n%3 == 0)
             n/=3;
-        }
-        else if (n%5 == 0){
+
+        while(n%5 == 0)
             n/=5;
-        }
-        else
+        
+        if(n > 1)
             return false;
-        }
         return true;
     }
     return false;
+
 }
