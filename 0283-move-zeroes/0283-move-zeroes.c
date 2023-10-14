4a628eb0d8dba7391993d79ea875e0ba40a7133e
GEONWOO PARK <101126026+x2-qp-cheese@users.noreply.github.com>
2023-10-15 00:43:34 +0900

    Time: 166 ms (14.81%), Space: 16 MB (7.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -1,18 +1,14 @@
 void moveZeroes(int* nums, int numsSize){
-    
-    int* res = (int*) malloc (sizeof(int) * numsSize);
-    int k = 0;
-    int j = numsSize-1;
-    
-    for(int i = 0; i < numsSize; i++){
-        if(nums[i] == 0)
-            res[j--] = 0;
-        else
-            res[k++] = nums[i];
-    }
-    
     for(int i = 0; i< numsSize; i++){
-        nums[i] = res[i];
+        if(nums[i]==0){
+            for(int j = i+1; j < numsSize; j++){
+                if(nums[j]){
+                    nums[i] = nums[j];
+                    nums[j] = 0;
+                    break;
+                }
+            }
+        }
     }
     
 }
