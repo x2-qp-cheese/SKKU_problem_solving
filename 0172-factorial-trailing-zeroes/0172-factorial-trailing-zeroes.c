edb3d57a912c0b38bfcac31eb575a39cf2e1921d
GEONWOO PARK <101126026+x2-qp-cheese@users.noreply.github.com>
2023-10-14 23:43:27 +0900

    Time: 4 ms (29.07%), Space: 6.4 MB (14.34%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.c
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.c
@@ -1,17 +1,6 @@
 int trailingZeroes(int n){
-//0의 개수
-//5의 개수만 확인하면 끝
-    int count = 0;
-    int tmp;
-    if(n == 0)
-        return count;
-    while(n){
-        tmp = n;
-        while(tmp % 5 == 0){
-            count++; 
-            tmp/=5;
-        }
-        n--;
-    }
-    return count;   
+    if(n < 4)
+        return 0;
+    return n/5 + trailingZeroes(n/5);
+    
 }
