899d4e40f42c80ee0320a9732949de14ff7d3149
GEONWOO PARK <101126026+x2-qp-cheese@users.noreply.github.com>
2023-10-15 23:23:48 +0900

    Time: 6 ms (56.19%), Space: 7.1 MB (32.47%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.c
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.c
@@ -1,24 +1,19 @@
 int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
-    // 해시 테이블을 초기화하고 최대 배열 크기를 계산합니다.
-    int hashtable[1001] = {0};
+    int map[1001] = {0};
     int maxsize = (nums1Size > nums2Size ? nums1Size : nums2Size);
 
-    // 결과 배열을 할당하고 반환 크기를 초기화합니다.
     int* res = (int*)malloc(sizeof(int) * maxsize);
     *returnSize = 0;
 
-    // nums1 배열을 해시 테이블에 저장합니다.
     for (int i = 0; i < nums1Size; i++) {
-        hashtable[nums1[i]]++;
+        map[nums1[i]]++;
     }
-
-    // nums2 배열을 순회하면서 중복된 요소를 찾아 결과 배열에 추가합니다.
+    
     for (int j = 0; j < nums2Size; j++) {
-        if (hashtable[nums2[j]] > 0) {
+        if (map[nums2[j]] > 0) {
             res[(*returnSize)++] = nums2[j];
-            hashtable[nums2[j]]--; // 중복 체크를 하지 않으므로 해시 테이블 값을 감소시키지 않아도 됩니다.
+            map[nums2[j]]--;
         }
     }
-
     return res;
 }
