d913d4343e99017c2c5b003d858ff5a4dcf3b6a0
GEONWOO PARK <101126026+x2-qp-cheese@users.noreply.github.com>
2023-12-05 04:06:07 +0900

    Time: 8 ms (98.63%), Space: 10.2 MB (34.25%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.c
+++ 0275-h-index-ii/0275-h-index-ii.c
@@ -1,10 +1,21 @@
 
-
-int hIndex(int* citations, int citationsSize) {
-    for(int i = 0; i < citationsSize; i++){
-        if(citations[i] >= citationsSize-i){
-            return citationsSize - i;
+int binarySearch(int* arr, int l, int r, int n){
+    int ans = 0;
+    while(l <= r){
+        int mid = l + (r-l) / 2;
+        if(arr[mid] >= n - mid)
+        {
+            ans = n-mid;
+            r = mid-1;
+        }
+        else
+        {
+            l = mid+1;
         }
     }
-    return 0;
+    return ans;
+}
+
+int hIndex(int* citations, int citationsSize) {
+    return binarySearch(citations, 0, citationsSize-1, citationsSize);
 }
