5e985ec702441256b1d02cf8e9e5280e9d71345d
GEONWOO PARK <101126026+x2-qp-cheese@users.noreply.github.com>
2023-12-06 01:08:08 +0900

    Time: 5 ms (20.68%), Space: 6.4 MB (36.62%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.c
@@ -0,0 +1,12 @@
+bool isPowerOfFour(int n) {
+    while(n){
+        if(n == 1){
+            return true;
+        }
+        if(n%4 != 0){
+            return false;
+        }
+        n/=4;
+    }
+    return 0;
+}
