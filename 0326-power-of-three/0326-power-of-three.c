82471b60851f90f63706445ce2005800489f64c3
GEONWOO PARK <101126026+x2-qp-cheese@users.noreply.github.com>
2023-12-06 01:00:21 +0900

    Time: 10 ms (77.50%), Space: 6.9 MB (84.80%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.c
@@ -0,0 +1,10 @@
+bool isPowerOfThree(int n) {
+    while(n > 0){
+        if(n == 1)
+            return true;
+        if(n%3!=0)
+            return false;
+        n/=3;
+    }
+    return false;
+}
